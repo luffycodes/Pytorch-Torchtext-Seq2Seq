07e16ccad61429d7c2cf974433451eb565eaac20
luffycodes <sonkar.iitk@gmail.com>
2018-02-27 10:38:09 -0600

    correct identation

--- model/Decoder.py
+++ model/Decoder.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from utils import *
 from model import *
 
+
 class Decoder(nn.Module):
     def __init__(self, vocab_size, embed_dim, hidden_dim, max_len, trg_soi):
         super(Decoder, self).__init__()
@@ -13,32 +14,31 @@ class Decoder(nn.Module):
         self.max_len = max_len
         self.vocab_size = vocab_size
         self.trg_soi = trg_soi
-        
-        self.embed = nn.Embedding(vocab_size, embed_dim)        
-        self.attention = Attention(hidden_dim) 
+
+        self.embed = nn.Embedding(vocab_size, embed_dim)
+        self.attention = Attention(hidden_dim)
         self.decodercell = DecoderCell(embed_dim, hidden_dim)
         self.dec2word = nn.Linear(hidden_dim, vocab_size)
 
-
     def forward(self, enc_h, prev_s, target=None):
-        '''
-        enc_h  : B x S x 2*H 
+        """
+        enc_h  : B x S x 2*H
         prev_s : B x H
-        '''
+        """
 
         if target is not None:
             batch_size, target_len = target.size(0), target.size(1)
-            
+
             dec_h = Variable(torch.zeros(batch_size, target_len, self.hidden_dim))
 
             if torch.cuda.is_available():
                 dec_h = dec_h.cuda()
 
-            target = self.embed(target)  
+            target = self.embed(target)
             for i in range(target_len):
-                ctx = self.attention(enc_h, prev_s)                     
-                prev_s = self.decodercell(target[:, i], prev_s, ctx)       
-                dec_h[:,i,:] = prev_s.unsqueeze(1)
+                ctx = self.attention(enc_h, prev_s)
+                prev_s = self.decodercell(target[:, i], prev_s, ctx)
+                dec_h[:, i, :] = prev_s.unsqueeze(1)
 
             outputs = self.dec2word(dec_h)
 
@@ -50,15 +50,15 @@ class Decoder(nn.Module):
             if torch.cuda.is_available():
                 target = target.cuda()
                 outputs = outputs.cuda()
-            
+
             for i in range(self.max_len):
-                target = self.embed(target).squeeze(1)              
-                ctx = self.attention(enc_h, prev_s)                 
+                target = self.embed(target).squeeze(1)
+                ctx = self.attention(enc_h, prev_s)
                 prev_s = self.decodercell(target, prev_s, ctx)
-                output = self.dec2word(prev_s) 
-                outputs[:,i,:] = output
+                output = self.dec2word(prev_s)
+                outputs[:, i, :] = output
                 target = output.topk(1)[1]
-            
+
         return outputs
 
 
@@ -66,23 +66,22 @@ class DecoderCell(nn.Module):
     def __init__(self, embed_dim, hidden_dim):
         super(DecoderCell, self).__init__()
 
-        self.input_weights = nn.Linear(embed_dim, hidden_dim*2)
-        self.hidden_weights = nn.Linear(hidden_dim, hidden_dim*2)
-        self.ctx_weights = nn.Linear(hidden_dim*2, hidden_dim*2)
+        self.input_weights = nn.Linear(embed_dim, hidden_dim * 2)
+        self.hidden_weights = nn.Linear(hidden_dim, hidden_dim * 2)
+        self.ctx_weights = nn.Linear(hidden_dim * 2, hidden_dim * 2)
 
         self.input_in = nn.Linear(embed_dim, hidden_dim)
         self.hidden_in = nn.Linear(hidden_dim, hidden_dim)
-        self.ctx_in = nn.Linear(hidden_dim*2, hidden_dim)
-
+        self.ctx_in = nn.Linear(hidden_dim * 2, hidden_dim)
 
     def forward(self, trg_word, prev_s, ctx):
-        '''
+        """
         trg_word : B x E
-        prev_s   : B x H 
+        prev_s   : B x H
         ctx      : B x 2*H
-        '''
+        """
         gates = self.input_weights(trg_word) + self.hidden_weights(prev_s) + self.ctx_weights(ctx)
-        reset_gate, update_gate = gates.chunk(2,1)
+        reset_gate, update_gate = gates.chunk(2, 1)
 
         reset_gate = F.sigmoid(reset_gate)
         update_gate = F.sigmoid(update_gate)
@@ -90,6 +89,5 @@ class DecoderCell(nn.Module):
         prev_s_tilde = self.input_in(trg_word) + self.hidden_in(prev_s) + self.ctx_in(ctx)
         prev_s_tilde = F.tanh(prev_s_tilde)
 
-        prev_s = torch.mul((1-reset_gate), prev_s) + torch.mul(reset_gate, prev_s_tilde)
+        prev_s = torch.mul((1 - reset_gate), prev_s) + torch.mul(reset_gate, prev_s_tilde)
         return prev_s
-        
