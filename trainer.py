88a913dbb430120666c4a4446409bf20d3590d10
luffycodes <sonkar.iitk@gmail.com>
2018-02-27 10:40:28 -0600

    correct identation

--- trainer.py
+++ trainer.py
@@ -32,7 +32,7 @@ class Trainer(object):
         # Path
         self.data_path = args.data_path
         self.sample_path = os.path.join('./samples/' + args.sample)
-        self.log_path = os.path.join('./logs/' + args.log) 
+        self.log_path = os.path.join('./logs/' + args.log)
 
         if not os.path.exists(self.sample_path): os.makedirs(self.sample_path)
         if not os.path.exists(self.log_path): os.makedirs(self.log_path)
@@ -50,12 +50,11 @@ class Trainer(object):
         self.iter_per_epoch = len(train_loader)
 
         # Log
-        self.logger = open(self.log_path+'/log.txt','w')
-        self.sample = open(self.sample_path+'/sample.txt','w')
+        self.logger = open(self.log_path + '/log.txt', 'w')
+        self.sample = open(self.sample_path + '/sample.txt', 'w')
         self.tf_log = Logger(self.log_path)
 
         self.build_model(vocabs)
-
 
     def build_model(self, vocabs):
         # build dictionaries
@@ -67,39 +66,40 @@ class Trainer(object):
 
         self.src_nword = len(self.src_vocab)
         self.trg_nword = len(self.trg_vocab)
-        
+
         # build the model
-        self.model = Seq2Seq(self.src_nword, self.trg_nword, self.num_layer, self.embed_dim, self.hidden_dim, 
-        					  self.max_len, self.trg_soi)
+        self.model = Seq2Seq(self.src_nword, self.trg_nword, self.num_layer, self.embed_dim, self.hidden_dim,
+                             self.max_len, self.trg_soi)
 
         # set the criterion and optimizer
         self.criterion = nn.NLLLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
         self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, 1, gamma=0.8)
-        
+
         if torch.cuda.is_available():
             self.model.cuda()
 
-        print (self.model)
-        print (self.criterion)
-        print (self.optimizer)
-
-
+        print(self.model)
+        print(self.criterion)
+        print(self.optimizer)
 
     def train(self):
         self.best_bleu = .0
-        
+
         for epoch in range(self.num_epoch):
-            #self.scheduler.step()
+            # self.scheduler.step()
             self.train_loss = AverageMeter()
             self.train_bleu = AverageMeter()
             start_time = time.time()
 
-            for i, batch in enumerate(tqdm(self.train_loader)):
+            for i, batch in enumerate(self.train_loader):
                 self.model.train()
 
-                src_input = batch.src[0]; src_length = batch.src[1]
-                trg_input = batch.trg[0][:,:-1]; trg_output=batch.trg[0][:,1:]; trg_length = batch.trg[1]
+                src_input = batch.src[0]
+                src_length = batch.src[1]
+                trg_input = batch.trg[0][:, :-1]
+                trg_output = batch.trg[0][:, 1:]
+                trg_length = batch.trg[1]
                 batch_size, trg_len = trg_input.size(0), trg_input.size(1)
 
                 decoder_logit = self.model(src_input, src_length.tolist(), trg_input)
@@ -123,7 +123,7 @@ class Trainer(object):
                 self.train_bleu.update(bleu_value, 1)
                 self.train_loss.update(loss.data[0], batch_size)
 
-                if i % 5000 == 0 and i != 0:
+                if i % 100 == 0 and i != 0:
                     self.print_train_result(epoch, i, start_time)
                     self.print_sample(batch_size, epoch, i, src_input, trg_output, pred)
                     self.eval(epoch, i)
@@ -136,24 +136,26 @@ class Trainer(object):
                         'epoch': epoch,
                         'train_iter': i,
                         'train_loss': self.train_loss.avg,
-                        'train_bleu': self.train_bleu.avg               
-                        }
+                        'train_bleu': self.train_bleu.avg
+                    }
                     for tag, value in info.items():
-                        self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch)+i+1)
+                        self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch) + i + 1)
 
             self.print_train_result(epoch, i, start_time)
-            self.print_sample(batch_size, epoch, i, src_input, trg_output, pred)           
-            self.eval(epoch, i)                
-
+            self.print_sample(batch_size, epoch, i, src_input, trg_output, pred)
+            self.eval(epoch, i)
 
     def eval(self, epoch, train_iter):
         self.model.eval()
         val_bleu = AverageMeter()
         start_time = time.time()
-        
+
         for i, batch in enumerate(tqdm(self.val_loader)):
-            src_input = batch.src[0]; src_length = batch.src[1]
-            trg_input = batch.trg[0][:,:-1]; trg_output=batch.trg[0][:,1:]; trg_length = batch.trg[1]
+            src_input = batch.src[0];
+            src_length = batch.src[1]
+            trg_input = batch.trg[0][:, :-1]
+            trg_output = batch.trg[0][:, 1:]
+            trg_length = batch.trg[1]
             batch_size, trg_len = trg_input.size(0), trg_input.size(1)
 
             decoder_logit = self.model(src_input, src_length.tolist())
@@ -161,7 +163,7 @@ class Trainer(object):
 
             # Compute BLEU score
             pred_sents = []
-            trg_sents = []            
+            trg_sents = []
             for j in range(batch_size):
                 pred_sent = self.get_sentence(tensor2np(pred[j]).argmax(axis=-1), 'trg')
                 trg_sent = self.get_sentence(tensor2np(trg_output[j]), 'trg')
@@ -169,36 +171,35 @@ class Trainer(object):
                 trg_sents.append(trg_sent)
             bleu_value = get_bleu(pred_sents, trg_sents)
             val_bleu.update(bleu_value, 1)
-               
-        self.print_valid_result(epoch, train_iter, val_bleu.avg, start_time)       
+
+        self.print_valid_result(epoch, train_iter, val_bleu.avg, start_time)
         self.print_sample(batch_size, epoch, train_iter, src_input, trg_output, pred)
 
         # Save model if bleu score is higher than the best 
         if self.best_bleu < val_bleu.avg:
-            self.best_bleu = val_bleu.avg        
+            self.best_bleu = val_bleu.avg
             checkpoint = {
-                    'model': self.model,
-                    'epoch': epoch
-                }
-            torch.save(checkpoint, self.log_path+'/Model_e%d_i%d_%.3f.pt' % (epoch, train_iter, val_bleu.avg))                 
+                'model': self.model,
+                'epoch': epoch
+            }
+            torch.save(checkpoint, self.log_path + '/Model_e%d_i%d_%.3f.pt' % (epoch, train_iter, val_bleu.avg))
 
-        # Logging tensorboard
+            # Logging tensorboard
         info = {
             'epoch': epoch,
             'train_iter': train_iter,
             'train_loss': self.train_loss.avg,
             'train_bleu': self.train_bleu.avg,
-            'bleu': val_bleu.avg                
-            }
+            'bleu': val_bleu.avg
+        }
 
         for tag, value in info.items():
-            self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch)+train_iter+1)
-
+            self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch) + train_iter + 1)
 
     def get_sentence(self, sentence, side):
         def _eos_parsing(sentence):
             if EOS_WORD in sentence:
-                return sentence[:sentence.index(EOS_WORD)+1]
+                return sentence[:sentence.index(EOS_WORD) + 1]
             else:
                 return sentence
 
@@ -210,37 +211,34 @@ class Trainer(object):
 
         return _eos_parsing(sentence)
 
-
     def print_train_result(self, epoch, train_iter, start_time):
         mode = ("=================================        Train         ====================================")
-        print (mode, '\n')
-        self.logger.write(mode+'\n')
-        self.sample.write(mode+'\n')
-        
-        message = "Train epoch: %d  iter: %d  train loss: %1.3f  train bleu: %1.3f  elapsed: %1.3f " % (
-        epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, time.time() - start_time)
-        print (message, '\n\n')
-        self.logger.write(message+'\n\n')   
+        print(mode, '\n')
+        self.logger.write(mode + '\n')
+        self.sample.write(mode + '\n')
 
+        message = "Train epoch: %d  iter: %d  train loss: %1.3f  train bleu: %1.3f  elapsed: %1.3f " % (
+            epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, time.time() - start_time)
+        print(message, '\n\n')
+        self.logger.write(message + '\n\n')
 
     def print_valid_result(self, epoch, train_iter, val_bleu, start_time):
         mode = ("=================================        Validation         ====================================")
-        print (mode, '\n')
-        self.logger.write(mode+'\n')
-        self.sample.write(mode+'\n')
+        print(mode, '\n')
+        self.logger.write(mode + '\n')
+        self.sample.write(mode + '\n')
 
         message = "Train epoch: %d  iter: %d  train loss: %1.3f  train_bleu:  %1.3f  val bleu score: %1.3f  elapsed: %1.3f " % (
-        epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, val_bleu, time.time() - start_time)
-        print (message, '\n\n' )
-        self.logger.write(message+'\n\n')
-        
+            epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, val_bleu, time.time() - start_time)
+        print(message, '\n\n')
+        self.logger.write(message + '\n\n')
 
     def print_sample(self, batch_size, epoch, train_iter, source, target, pred):
 
         def _write_and_print(message):
             for x in message:
-                self.sample.write(x+'\n')
-            print ((" ").join(message))
+                self.sample.write(x + '\n')
+            print((" ").join(message))
 
         random_idx = randomChoice(batch_size)
         src_sample = self.get_sentence(tensor2np(source)[random_idx], 'src')
@@ -249,10 +247,10 @@ class Trainer(object):
 
         src_message = ["Source Sentence:    ", (" ").join(src_sample), '\n']
         trg_message = ["Target Sentence:    ", (" ").join(trg_sample), '\n']
-        pred_message =  ["Generated Sentence: ", (" ").join(pred_sample), '\n']
+        pred_message = ["Generated Sentence: ", (" ").join(pred_sample), '\n']
 
         message = "Train epoch: %d  iter: %d " % (epoch, train_iter)
-        self.sample.write(message+'\n')
+        self.sample.write(message + '\n')
         _write_and_print(src_message)
         _write_and_print(trg_message)
         _write_and_print(pred_message)
