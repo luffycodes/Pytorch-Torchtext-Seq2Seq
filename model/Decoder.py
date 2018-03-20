b032d4d0a948b0dfa583192087cc54464540b18b
luffycodes <sonkar.iitk@gmail.com>
2018-03-20 12:46:58 -0500

    1. moving function inside model

--- model/Decoder.py
+++ model/Decoder.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from utils import *
 from model import *
 
 '''
@@ -28,7 +27,7 @@ class Decoder(nn.Module):
 
     def forward(self, target, trg_length=None, hidden=None):
         batch_size = target.size(0)
-        sorted_inputs, sorted_seq_len, restoration_indices, _ = sort_batch_by_length(target, Variable(torch.FloatTensor(trg_length)))
+        sorted_inputs, sorted_seq_len, restoration_indices, _ = self.sort_batch_by_length(target, Variable(torch.FloatTensor(trg_length)))
 
         src_embed = self.embedding(sorted_inputs)
 
@@ -36,7 +35,7 @@ class Decoder(nn.Module):
             h_size = (self.num_layers *2, batch_size, self.hidden_dim)
             enc_h_0 = Variable(src_embed.data.new(*h_size).zero_(), requires_grad=False)
 
-        seq_length = [int(x) for x in sorted_seq_len.cpu().data.numpy().tolist()]
+        seq_length = [int(x) for x in sorted_seq_len.data.numpy().tolist()]
         src_embed = nn.utils.rnn.pack_padded_sequence(src_embed, seq_length, batch_first=True)
         enc_h, enc_h_t = self.gru(src_embed, enc_h_0)
 
@@ -52,3 +51,47 @@ class Decoder(nn.Module):
         last_layer_state = unsorted_state[:, -last_state_index:, :]
 
         return enc_h, last_layer_state
+
+    def sort_batch_by_length(self, tensor: torch.autograd.Variable, sequence_lengths: torch.autograd.Variable):
+        """
+        Sort a batch first tensor by some specified lengths.
+
+        Parameters
+        ----------
+        tensor : Variable(torch.FloatTensor), required.
+            A batch first Pytorch tensor.
+        sequence_lengths : Variable(torch.LongTensor), required.
+            A tensor representing the lengths of some dimension of the tensor which
+            we want to sort by.
+
+        Returns
+        -------
+        sorted_tensor : Variable(torch.FloatTensor)
+            The original tensor sorted along the batch dimension with respect to sequence_lengths.
+        sorted_sequence_lengths : Variable(torch.LongTensor)
+            The original sequence_lengths sorted by decreasing size.
+        restoration_indices : Variable(torch.LongTensor)
+            Indices into the sorted_tensor such that
+            ``sorted_tensor.index_select(0, restoration_indices) == original_tensor``
+        permuation_index : Variable(torch.LongTensor)
+            The indices used to sort the tensor. This is useful if you want to sort many
+            tensors using the same ordering.
+        """
+
+        if not isinstance(tensor, Variable) or not isinstance(sequence_lengths, Variable):
+            raise Exception("Both the tensor and sequence lengths must be torch.autograd.Variables.")
+
+        sorted_sequence_lengths, permutation_index = sequence_lengths.sort(0, descending=True)
+        sorted_tensor = tensor.index_select(0, permutation_index)
+
+        # This is ugly, but required - we are creating a new variable at runtime, so we
+        # must ensure it has the correct CUDA vs non-CUDA type. We do this by cloning and
+        # refilling one of the inputs to the function.
+        index_range = sequence_lengths.data.clone().copy_(torch.arange(0, len(sequence_lengths)))
+        # This is the equivalent of zipping with index, sorting by the original
+        # sequence lengths and returning the now sorted indices.
+        index_range = Variable(index_range.long()).cuda()
+        _, reverse_mapping = permutation_index.sort(0, descending=False)
+        restoration_indices = index_range.index_select(0, reverse_mapping)
+
+        return sorted_tensor, sorted_sequence_lengths, restoration_indices, permutation_index
