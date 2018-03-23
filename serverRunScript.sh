19fe56bb9dac1d92828a76ea9779d831d6297bbe
luffycodes <sonkar.iitk@gmail.com>
2018-03-23 14:49:15 -0500

    1. serverRunScript update

--- serverRunScript.sh
+++ serverRunScript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+cd /root/pythonProjects/
+
+rm -rf /root/pythonProjects/Pytorch-Torchtext-Seq2Seq
 rm -rf /dataroot/Pytorch-Torchtext-Seq2Seq-Data/model_results/tfEvents
 rm /dataroot/Pytorch-Torchtext-Seq2Seq-Data/model_results/console.log
-rm nohup.out
-git pull origin nodecoder
+
+git clone https://github.com/luffycodes/Pytorch-Torchtext-Seq2Seq.git --branch nodecoder --single-branch
+
+cd Pytorch-Torchtext-Seq2Seq
+
 nohup python main.py &
 tail -f nohup.out
