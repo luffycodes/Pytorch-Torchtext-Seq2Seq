a9e8485262a362121be90571e0c5e49179f9d5dd
luffycodes <sonkar.iitk@gmail.com>
2018-03-26 22:39:02 -0500

    1. bug in diagonal loss

--- trainer.py
+++ trainer.py
@@ -130,7 +130,7 @@ class Trainer(object):
                     # Logging tensorboard
                     info = {
                         'train_loss': self.train_loss.avg,
-                        'diagonal_loss': self.diagonal_loss
+                        'diagonal_loss': self.diagonal_loss.avg
                     }
                     self.tf_log.add_scalars('Training loss', info, (epoch * self.iter_per_epoch) + i + 1)
 
