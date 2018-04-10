7814cc25d97ab5905540abf7e08ddb05e82c8ca6
luffycodes <sonkar.iitk@gmail.com>
2018-04-10 18:08:14 -0500

    1. Bug in pairwise distance

--- model/Seq2Seq.py
+++ model/Seq2Seq.py
@@ -9,6 +9,29 @@ from model import Encoder
 from model import *
 
 
+def pairwise_distances(x, y=None):
+    """
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    """
+    x_norm = (x ** 2).sum(1).view(-1, 1)
+    if y is not None:
+        y_t = torch.transpose(y, 0, 1)
+        y_norm = (y ** 2).sum(1).view(1, -1)
+    else:
+        y_t = torch.transpose(x, 0, 1)
+        y_norm = x_norm.view(1, -1)
+
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
+    # Ensure diagonal is zero if x=y
+    # if y is None:
+    #     dist = dist - torch.diag(dist.diag)
+    return torch.clamp(dist, 0.0, np.inf)
+
+
 class Seq2Seq(nn.Module):
     def __init__(self, src_nword, trg_nword, num_layer, embed_dim, hidden_dim, max_len, trg_soi, bi_dir):
         super(Seq2Seq, self).__init__()
@@ -39,8 +62,7 @@ class Seq2Seq(nn.Module):
 
             # loss = torch.mm(bi_enc_h_t, bi_dec_h_t.transpose(0, 1))
 
-            bi_enc_h_t_Minus_bi_dec_h_t = bi_enc_h_t - bi_dec_h_t
-            loss = torch.mm(bi_enc_h_t_Minus_bi_dec_h_t, bi_enc_h_t_Minus_bi_dec_h_t.transpose(0, 1))
+            loss = pairwise_distances(bi_enc_h_t, bi_dec_h_t)
             nce_loss = 0
 
             for x in range(0, loss.size()[0]):
